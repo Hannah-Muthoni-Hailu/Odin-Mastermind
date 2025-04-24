c28010203ad0688acfa3b46f48a7e15a7ecc5343
Hannah-Muthoni-Hailu <muthonihannahhailu@gmail.com>
2025-04-24 10:11:58 +0300

    Ask user if they want to be codemaker or codebreaker

--- main.rb
+++ main.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 require_relative 'board'
 require_relative 'Players/human'
 require_relative 'Players/computer'
@@ -6,9 +8,18 @@ def game_play
   # Initialize new board
   board = Board.new
 
+  # Ask player if they want to be codemaker or codebreaker
+  puts "Do you want to be codemaker or codebreaker? (Select 'M' for codemaker and 'B' for codebreaker)"
+  choice = gets.chomp.upcase
+
   # Initialize code maker and breaker
-  code_maker = Computer.new
-  code_breaker = Human.new
+  if choice == 'M'
+    code_maker = Computer.new
+    code_breaker = Human.new
+  else
+    code_maker = Human.new
+    code_breaker = Computer.new
+  end
 
   # Set code
   board.save_code(code_maker.create_code)
@@ -20,19 +31,19 @@ def game_play
     # Obtain guess and update board with guess
     player_guess = code_breaker.guess
     board.update_guess(player_guess)
-    
+
     # Obtain pegs and update board
     pegs = code_maker.check_guess(player_guess)
     board.update_pegs(pegs)
 
     # Check win
-    if board.win_check then return "Code Breaker has won" end
+    return 'Code Breaker has won' if board.win_check
   end
 
   # Code Maker has won if loop exits
   board.display
   p board.code # Display code to code breaker
-  return "Code Maker has won"
+  'Code Maker has won'
 end
 
 puts game_play
